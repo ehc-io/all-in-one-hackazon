fa854222ddf5b83b432ab97757dc044b90041a48
ehc-io <eduardo@ehc.io>
2019-01-31 18:33:35 -0200

    added script to run the container

--- run_hackazon.sh
+++ run_hackazon.sh
@@ -10,7 +10,7 @@ fi
 # kill current instances
 # sudo killall apache2 &>/dev/null
 # sudo docker kill $(docker ps -q) &>/dev/null
-docker kill hackazon 
+# docker kill hackazon 
 # run hackazon
 docker run --name hackazon --rm -it -p $1:80 hackazon
 #
